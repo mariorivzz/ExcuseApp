39067de0cd55ae8608456251a3aa1713d89fbcb7
mariorivzz <mariomariorivasrivas@gmail.com>
2025-07-05 00:20:19 +0200

    Firebase quitado hay que cambiar credenciales

--- config/firebase.ts
+++ config/firebase.ts
@@ -9,13 +9,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Platform } from 'react-native';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBUAFfKxHnO3VxL98U6T9KcqfAsY3GWGhY",
-  authDomain: "excuseapp-833f8.firebaseapp.com",
-  projectId: "excuseapp-833f8",
-  storageBucket: "excuseapp-833f8.firebasestorage.app",
-  messagingSenderId: "105857421010",
-  appId: "1:105857421010:web:e35a0f2a3216a91f4fe70b",
-  measurementId: "G-7EM0XR3XFF",
+  // todo: Reemplaza con tu configuración de Firebase
 };
 
 const app = initializeApp(firebaseConfig);
